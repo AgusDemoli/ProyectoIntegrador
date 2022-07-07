0bea85babc2c9eaf1b407625b9afc6a857e40ff7
Agustin Demoli <agustindemoli@gmail.com>
2022-07-07 13:10:57 -0300

    Front End maquetado

--- proyectoPortfolio_AgustinDemoli/src/app/app.module.ts
+++ proyectoPortfolio_AgustinDemoli/src/app/app.module.ts
@@ -7,6 +7,13 @@ import { HeaderComponent } from './components/header/header.component';
 import { APlogoComponent } from './components/aplogo/aplogo.component';
 import { SocialMediaComponent } from './components/social-media/social-media.component';
 import { BannerComponent } from './components/banner/banner.component';
+import { AboutmeComponent } from './components/aboutme/aboutme.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { EducationComponent } from './components/education/education.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -14,11 +21,27 @@ import { BannerComponent } from './components/banner/banner.component';
     HeaderComponent,
     APlogoComponent,
     SocialMediaComponent,
-    BannerComponent
+    BannerComponent,
+    AboutmeComponent,
+    ExperienceComponent,
+    EducationComponent,
+    SkillsComponent,
+    ProjectsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({
+      /*
+      radius: 100,
+      outerStrokeWidth: 16,
+      innerStrokeWidth: 8,
+      outerStrokeColor: "#78C000",
+      innerStrokeColor: "#C7E596",
+      animationDuration: 300,
+      */
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
